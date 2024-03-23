ebf4ceb1f060cff0f500f7fe4280eb20568eea73
Soroush Nasiriany <sornasir324@gmail.com>
2024-03-23 16:06:18 -0700

    distinguish gripper and arm action space

--- r2d2/evaluation/eval_launcher_robomimic.py
+++ r2d2/evaluation/eval_launcher_robomimic.py
@@ -44,8 +44,6 @@ def eval_launcher(variant, run_id, exp_id):
     # ckpt_path = "~/expdata/r2d2/im/bc_xfmr/05-25-c2t-and-t2c-cans/ds_pnp-t2c-cans_predfutureacs_False/2023-05-25-22-34-36/models/model_epoch_200.pth"
 
     ckpt_path = variant["ckpt_path"]
-    task = variant["task"]
-    layout_id = variant["layout_id"]
 
     device = TorchUtils.get_torch_device(try_to_use_cuda=True)
     ckpt_dict = FileUtils.maybe_dict_from_checkpoint(ckpt_path=ckpt_path)
@@ -56,62 +54,36 @@ def eval_launcher(variant, run_id, exp_id):
     imsize = 128 #ckpt_dict["shape_metadata"]["all_shapes"]["camera/image/varied_camera_1_left_image"][2]
     print(imsize)
 
-    ### custom code for loading bc_ret ckpt ###
-    # ISL_CKPT_PATH = "~/expdata/r2d2/im/isl/05-28-t2c-cans/ds_pnp-t2c-cans-84/2023-05-28-19-51-46/models/model_epoch_10.pth"
-    # config = json.loads(ckpt_dict["config"])
-    # config["algo"]["retrieval"]["model_ckpt_path"] = ISL_CKPT_PATH
-    # config["train"]["data_test"] = config["train"]["data_prior"][:3]
-    # ckpt_dict["config"] = json.dumps(config)
-
-    """
-    ### set up the test tasks ###
-    DATASETS = OrderedDict()
-    DATASETS["t2c-cans"] = dict(include_list=[{"env": "pnp-table-to-cab-cans"}])
-    DATASETS["t2c-chips"] = dict(include_list=[{"env": "pnp-table-to-cab-chips"}])
-    DATASETS["t2c-granola"] = dict(include_list=[{"env": "pnp-table-to-cab-granola"}])
-    
-    DATASETS["s2t-cans"] = dict(include_list=[{"env": "pnp-sink-to-table-cans"}])
-    DATASETS["s2t-chips"] = dict(include_list=[{"env": "pnp-sink-to-table-chips"}])
-    DATASETS["s2t-granola"] = dict(include_list=[{"env": "pnp-sink-to-table-granola"}])
-
-    DATASETS["t2s-cans"] = dict(include_list=[{"env": "pnp-table-to-sink-cans"}])
-    DATASETS["t2s-chips"] = dict(include_list=[{"env": "pnp-table-to-sink-chips"}])
-    DATASETS["t2s-granola"] = dict(include_list=[{"env": "pnp-table-to-sink-granola"}])
-
-    ### get datasets for task ###
-
-    if task is None:
-        ds = config["train"]["data_prior"]
-        config["train"]["data_test"] = ds
-        config["train"]["data"] = ds
-    else:
-        ds_cfg = deepcopy(DATASETS[task])
-        if config["algo"]["retrieval"]["exclude_same_layout_pairs"]:
-            ds_cfg["exclude_list"] = ds_cfg.get("exclude_list", [])
-            ds_cfg["exclude_list"].append({"layout_id": [layout_id]})
-        ds = get_r2d2_datasets(
-            imsize=imsize,
-            **ds_cfg,
-        )
-        
-        ### cap number of datasets to 16 to maintain memory requirements ###
-        rng = np.random.default_rng(seed=0)
-        rng.shuffle(ds)
-        ds = ds[:16]
-
-        config["train"]["data_test"] = ds
-        config["train"]["data_prior"] = None
-        config["train"]["data"] = ds
-    """
-
     ckpt_dict["config"] = json.dumps(config)
     policy, _ = FileUtils.policy_from_checkpoint(ckpt_dict=ckpt_dict, device=device, verbose=True)
+
+    # determine the action space (relative or absolute)
+    action_keys = config["train"]["action_keys"]["r2d2"]
+    if "action/rel_pos" in action_keys:
+        action_space = "cartesian_velocity"
+        for k in action_keys:
+            assert not k.startswith("action/abs_")
+    elif "action/abs_pos" in action_keys:
+        action_space = "cartesian_position"
+        for k in action_keys:
+            assert not k.startswith("action/rel_")
+    else:
+        raise ValueError
+
+    # determine the action space for the gripper
+    if "action/gripper_velocity" in action_keys:
+        gripper_action_space = "velocity"
+    elif "action/gripper_position" in action_keys:
+        gripper_action_space = "position"
+    else:
+        raise ValueError
 
     # Prepare Policy Wrapper #
     # data_processing_kwargs = variant.get("data_processing_kwargs", {})
     data_processing_kwargs = dict(
         timestep_filtering_kwargs=dict(
-            action_space="cartesian_velocity",
+            action_space=action_space, #"cartesian_velocity",
+            gripper_action_space=gripper_action_space,
             robot_state_keys=["cartesian_position", "gripper_position", "joint_positions"],
             camera_extrinsics=[],
         ),
@@ -140,9 +112,6 @@ def eval_launcher(variant, run_id, exp_id):
         eval_mode=True,
     )
 
-    # Prepare Environment #
-    policy_action_space = policy_timestep_filtering_kwargs["action_space"]
-
     # camera_kwargs = variant.get("camera_kwargs", {})
     camera_kwargs = dict(
         hand_camera=dict(image=True, concatenate_images=False, resolution=(imsize, imsize), resize_func="cv2"),
@@ -155,7 +124,11 @@ def eval_launcher(variant, run_id, exp_id):
     # print("sleeping...")
     # import time; time.sleep(100)
 
-    env = RobotEnv(action_space=policy_action_space, camera_kwargs=policy_camera_kwargs)
+    env = RobotEnv(
+        action_space=policy_timestep_filtering_kwargs["action_space"],
+        gripper_action_space=policy_timestep_filtering_kwargs["gripper_action_space"],
+        camera_kwargs=policy_camera_kwargs
+    )
     controller = VRPolicy()
 
     # Launch GUI #
